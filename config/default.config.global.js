cb16459de17b6e442116b53aef8ed76121cadfac
Manoj Naik <mnaik@juniper.net>
2014-11-10 14:31:58 +0530

    Disabling Virtual Routers page in Config-Infrastructure as well.
    Change-Id: I16eef0c782324875c492dac97abfd25213d470ae

--- config/default.config.global.js
+++ config/default.config.global.js
@@ -264,7 +264,7 @@ config.physicaldevices.interface_delimiters = ['.', ':'];
 * Below are the disabled list of UI features.
 *****************************************************************************/
 config.features = {};
-config.features.disabled = ['config_pd','mon_infra_underlay'];
+config.features.disabled = ['config_pd','config_pd_virtualRouters','mon_infra_underlay'];
 
 /*****************************************************************************
 * Below are the configurations used only for ui
